6b6eef83f25c0fd8885298c566afa862e993ea36
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-06-28 09:39:04 +0100

    activated the delete chats btn

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const chatInput = document.querySelector('#chat-input')
 const sendButton = document.querySelector('#send-btn')
 const chatContainer = document.querySelector('.chat-container')
 const themeButton = document.querySelector('#theme-btn')
+const deleteButton = document.querySelector('#delete-btn')
 
 let userText = null;
 const oops = "";
@@ -114,5 +115,13 @@ themeButton.addEventListener("click", () =>{
     localStorage.setItem("theme-color", themeButton.innerText);
     themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 });
+
+deleteButton.addEventListener("click", () => {
+    // Remove the chats from local storage and call loadDataFromLocalStorage function
+    if(confirm("Are you sure you want to delete all the chats?")){
+        localStorage.removeItem("all-chats");
+        loadDataFromLocalStorage();
+    }
+})
 
 sendButton.addEventListener("click", handleOutgoingChat);
